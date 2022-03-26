050427dadb27f257e5154730b3db6704c4baa9f6
Muhammad Bilal Jamil <70377341+mbjamil92@users.noreply.github.com>
2022-03-26 16:52:29 -0400

    Add files via upload

---
+++ Lab02_ETL_Type2SCD/Lab2_Group2_py_batch.bat
@@ -0,0 +1,3 @@
+@echo off
+"C:\Program Files\Python39\python.exe" "C:\Users\anike\OneDrive\Documents\YeshivaUniversity\DAV\Spring_2022\Information_Architectures\M_7\M7_Lab\Part 1.1_Lab\email_example.py"
+pause
---
+++ Lab02_ETL_Type2SCD/Lab2_Group2_sql_batch.bat
@@ -0,0 +1,3 @@
+@echo off
+"C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql.exe" --host=localhost --user=root --password=H --database=dav6100_db_2 < "C:\Users\anike\OneDrive\Documents\YeshivaUniversity\DAV\Spring_2022\Information_Architectures\M_7\M7_Lab\Part 1.1_Lab\dimension_output_example.sql"
+pause
