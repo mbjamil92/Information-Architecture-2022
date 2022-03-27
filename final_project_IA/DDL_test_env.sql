4207c68fb9a2a0a1cddec8302afb61e459a63778
Muhammad Bilal Jamil <70377341+mbjamil92@users.noreply.github.com>
2022-03-27 17:27:54 -0400

    RDS setup done

---
+++ final_project_IA/DDL_test_env.sql
@@ -0,0 +1,4 @@
+# DDL for test environment for project:
+
+CREATE DATABASE `IA_Final_Project_NY_Transportation_Test` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
