40513ec79be6565fe731dc4a6484d0ad4525779a
Muhammad Bilal Jamil <70377341+mbjamil92@users.noreply.github.com>
2022-05-13 08:39:35 -0400

    Delete DDL_prod_env.sql

--- final_project_IA/DDL_prod_env.sql
+++
@@ -1,2 +0,0 @@
-# DDL for prod environment for project:
-CREATE DATABASE `IA_Final_Project_NY_Transportation_Prod` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
